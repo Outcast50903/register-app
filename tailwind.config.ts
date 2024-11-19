9e30f95c4b83d6e53b6e5838b64b2be9eb6f387b
Diego García <dgarciavallejos@gmail.com>
2024-11-18 21:58:53 -0300

    feat(lint): Consolidate ThemeProvider imports for cleaner code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-const { fontFamily } = require("tailwindcss/defaultTheme")
+import { fontFamily } from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -72,5 +72,6 @@ module.exports = {
       },
     },
   },
+  // eslint-disable-next-line @typescript-eslint/no-require-imports
   plugins: [require("tailwindcss-animate")],
-}
+};
