eaf373c35b6c73d782ce4557c46184f3b11b0bfa
Diego García <dgarciavallejos@gmail.com>
2024-11-18 23:28:01 -0300

    fix(env): Replace env import with process.env for DATABASE_URL in drizzle configuration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,11 @@
 import { defineConfig } from "drizzle-kit";
 
-import { env } from "@/env.mjs";
-
 export default defineConfig({
   dialect: "postgresql",
   schema: "./src/db/schema",
   out: "./src/db/migrations",
   dbCredentials: {
-    url: env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
   tablesFilter: ["app_*"],
   entities: {
