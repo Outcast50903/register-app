09236c807ae356520030c96ea59f92dc35fc0daf
Diego García <dgarciavallejos@gmail.com>
2024-11-18 21:46:12 -0300

    feat(lint): Enhance linting and commit conventions with validate-branch-name

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,5 +1,21 @@
 const Configuration = {
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "type-enum": [
+      2,
+      "always",
+      ["feat", "fix", "docs", "style", "refactor", "perf", "test", "chore"],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "scope-case": [2, "always", "kebab-case"],
+    "scope-empty": [2, "never"],
+    "subject-case": [2, "always", "sentence-case"],
+    "subject-full-stop": [2, "never", "."],
+    "subject-empty": [2, "never"],
+    "subject-min-length": [2, "always", 1],
+  },
 };
 
 export default Configuration;
